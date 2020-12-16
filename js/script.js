630585742c6b97f379926a85a05e5538362693ed
Simone-Ortu <simo.28.09o@gmail.com>
2020-12-16 15:54:11 +0100

    logica js base

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert('ciao')
+// Gioco dei dadi
+// Generare un numero random da 1  a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+
+
+var dado1 = Math.floor(Math.random() * 6)+1;
+console.log(dado1);
+var dado2 = Math.floor(Math.random() * 6)+1;
+console.log(dado2);
+
+if (dado1>dado2){
+    alert('dado1 wins');
+}
+else if (dado2>dado1){
+    alert('dado2 wins');
+}
+else{
+    alert('draw');
+}
